643eb41736734d4deba8cabdda2a4311f481dcb4
light <dohuyphi9xpro@gmail.com>
2025-07-05 08:27:46 +0700

    khoi tao noi dung dau tien

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');  // Nhúng thư viện morgan
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));  // middleware để ghi log
+
+app.get('/', (req, res) => {
+  res.send('Trang chủ');
+});
+
+app.get('/light', (req, res) => {
+  res.send('Yagami Light');
+});
+
+app.listen(port, () => {
+  console.log(`Server chạy tại http://localhost:${port}`);
+});
